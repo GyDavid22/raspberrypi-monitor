ab7575bdd154f4c6b43ec356831a066f2d2c165e
David Gyenes <gyenesdavid@gmail.com>
2025-11-08 12:49:16 +0100

    Backend code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,26 @@
-(() => {
-    console.log('Hello world!');
-})();
+import express from 'express';
+import * as fs from 'fs';
+
+let lastDate: string | undefined;
+
+const app = express();
+
+app.get('/ping', (_req, res) => {
+    lastDate = new Date().toISOString();
+    res.sendStatus(200);
+});
+
+app.get('/date', (_req, res) => {
+    if (lastDate) {
+        res.type('text/plain').status(200).send(lastDate);
+    } else {
+        res.status(404).send();
+    }
+});
+
+app.get('/', (_req, res) => {
+    const html = fs.readFileSync('index.html', 'utf8');
+    res.type('html').status(200).send(html);
+});
+
+app.listen(10000);
